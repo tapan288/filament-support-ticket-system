766e4386e2dd62f13242ebd508315896a590374b
tapan288 <tapansharma69@gmail.com>
2023-01-13 22:55:04 +0545

    define PermissionPolicy and implement checks accordingly

---
+++ app/Policies/PermissionPolicy.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Permission;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PermissionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->hasPermission('permission_access');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Permission  $permission
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Permission $permission)
+    {
+        return $user->hasPermission('permission_show');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->hasPermission('permission_create');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Permission  $permission
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Permission $permission)
+    {
+        return $user->hasPermission('permission_edit');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Permission  $permission
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Permission $permission)
+    {
+        return $user->hasPermission('permission_delete');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Permission  $permission
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Permission $permission)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Permission  $permission
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Permission $permission)
+    {
+        //
+    }
+
+    public function deleteAny(User $user, Permission $permission)
+    {
+        return $user->hasPermission('permission_delete');
+    }
+}
