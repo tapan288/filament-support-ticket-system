4b3b9d7498247373e8d261deedb894e2e9c12d98
tapan288 <tapansharma69@gmail.com>
2023-01-13 23:12:43 +0545

    add more policy checks

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,8 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $with = ['roles.permissions'];
+
     /**
      * The attributes that should be hidden for serialization.
      *
