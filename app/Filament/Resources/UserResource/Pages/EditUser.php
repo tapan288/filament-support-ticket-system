88b135a0d231ca9baad89d017977c81d7007d82d
tapan288 <tapansharma69@gmail.com>
2023-07-04 21:57:01 +0545

    implement custom action to update user's password

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Pages\Actions;
+use Illuminate\Support\Facades\Hash;
+use App\Filament\Resources\UserResource;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
@@ -13,6 +16,27 @@ class EditUser extends EditRecord
     protected function getActions(): array
     {
         return [
+            Actions\Action::make('updaePassword')
+                ->label('Update Password')
+                ->form([
+                    TextInput::make('password')
+                        ->password()
+                        ->confirmed()
+                        ->disableAutocomplete(),
+                    TextInput::make('password_confirmation')
+                        ->password()
+                        ->disableAutocomplete(),
+                ])
+                ->action(function (array $data) {
+                    $this->record->update([
+                        'password' => Hash::make($data['password']),
+                    ]);
+
+                    Notification::make()
+                        ->success()
+                        ->title('Password Updated Successfully')
+                        ->send();
+                }),
             Actions\DeleteAction::make(),
         ];
     }
