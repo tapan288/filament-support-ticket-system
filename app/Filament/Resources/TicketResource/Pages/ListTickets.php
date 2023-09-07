a04935663b06cae9d1737499358978a32b4c1125
tapan288 <tapansharma69@gmail.com>
2023-09-07 13:56:17 +0545

    add header widgets on tickets index page

--- app/Filament/Resources/TicketResource/Pages/ListTickets.php
+++ app/Filament/Resources/TicketResource/Pages/ListTickets.php
@@ -2,11 +2,12 @@
 
 namespace App\Filament\Resources\TicketResource\Pages;
 
+use App\Filament\Widgets\StatsOverview;
+use App\Models\Role;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\TicketResource;
-use App\Models\Role;
 
 class ListTickets extends ListRecords
 {
@@ -21,6 +22,13 @@ class ListTickets extends ListRecords
 
     protected function getTableQuery(): Builder
     {
-        return auth()->user()->hasRole(Role::ROLES['Admin']) ?  parent::getTableQuery() : parent::getTableQuery()->where('assigned_to', auth()->id());
+        return auth()->user()->hasRole(Role::ROLES['Admin']) ? parent::getTableQuery() : parent::getTableQuery()->where('assigned_to', auth()->id());
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            StatsOverview::make(),
+        ];
     }
 }
--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use App\Models\Label;
 use App\Models\Ticket;
 use App\Models\Category;
-use Filament\Widgets\StatsOverviewWidget\Card;
+use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatsOverview extends BaseWidget
@@ -17,12 +17,12 @@ class StatsOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
-            Card::make('Total Tickets', Ticket::count()),
-            Card::make('Total Agents', User::whereHas('roles', function ($query) {
+            Stat::make('Total Tickets', Ticket::count()),
+            Stat::make('Total Agents', User::whereHas('roles', function ($query) {
                 $query->where('title', Role::ROLES['Agent']);
             })->count()),
-            Card::make('Total Categories', Category::count()),
-            Card::make('Total Labels', Label::count()),
+            Stat::make('Total Categories', Category::count()),
+            Stat::make('Total Labels', Label::count()),
         ];
     }
 }
